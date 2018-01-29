9fbf3b6ca2eeba2cdc77a8c07106f54f7a9e1053
David Randolph <dave@tuningbell.com>
2018-01-28 23:24:51 -0600

    is_monophonic()

--- DCorpus/DCorpus.py
+++ DCorpus/DCorpus.py
@@ -21,27 +21,57 @@ __author__ = 'David Randolph'
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-import music21
+from music21 import *
 from Dactyler import Constant
 
 
 class DPart:
-    def __init__(self, music21_score):
-        self.score = music21_score
+    def __init__(self, music21_stream, segmenter=None):
+        self.stream = music21_stream
+        self.segmenter = segmenter
+
+    def _quantize_stream(self):
+        return self.stream
+
+    def is_monophonic(self):
+        if self.stream.flat.getElementsByClass(chord.Chord):
+            return False
+        notes = self.stream.flat.getElementsByClass(note.Note)
+        for i in range(len(notes)):
+            # print("{0}: {1}".format(str(notes[i].offset), str(notes[i].pitch)))
+            start = notes[i].offset
+            end = start + notes[i].duration.quarterLength
+            notes_in_range = self.stream.flat.getElementsByOffset(
+                offsetStart=start, offsetEnd=end,
+                includeEndBoundary=False,
+                mustBeginInSpan=False,
+                includeElementsThatEndAtStart=False,
+                classList=[note.Note]
+            )
+            if len(notes_in_range) > 1:
+                return False
+        return True
+
+    def get_stream(self, quantize=False):
+        if quantize:
+            return self._quantize_stream()
+        return self.stream
 
 
 class DScore:
-    def __init__(self, music21_score=None, abc_handle=None):
-        if music21_score:
-            self.score = music21_score
+    def __init__(self, music21_stream=None, abc_handle=None):
+        if music21_stream:
+            self.combined_d_part = DPart(music21_stream=music21_stream)
+            self.score = music21_stream
             meta = self.score[0]
             self.title = meta.title
         elif abc_handle:
-            self.score = abc_handle.tokenProcess()
             self.title = abc_handle.getTitle()
+            music21_stream = abcFormat.translate.abcToStreamScore(abc_handle)
+            self.combined_d_part = DPart(music21_stream=music21_stream)
 
-            self.left_hand_d_part = None
-            self.right_hand_d_part = None
+            self.lower_d_part = None
+            self.upper_d_part = None
 
             ah_array = abc_handle.splitByVoice()
             voices = []
@@ -55,20 +85,41 @@ class DScore:
                     else:
                         headers = headers + ah
 
-            if len(voices) > 2:
-                raise Exception("Too many voices")
-            elif len(voices) == 2:
-                complete_rh_ah = headers + voices[0]  # First voice must be for right hand
-                # print([t.src for t in complete_rh_ah.tokens])
-                right_hand_part = complete_rh_ah.tokenProcess()
-                self.right_hand_d_part = DPart(music21_score=right_hand_part)
-                complete_lh_ah = headers + voices[1]  # Second voice must be for left hand
-                # print([t.src for t in complete_lh_ah.tokens])
-                left_hand_part = complete_lh_ah.tokenProcess()
-                self.left_hand_d_part = DPart(music21_score=left_hand_part)
+            if len(voices) == 2:
+                upper_ah = headers + voices[0]  # First voice must be for right hand
+                upper_stream = abcFormat.translate.abcToStreamScore(upper_ah)
+                self.upper_d_part = DPart(music21_stream=upper_stream)
+                lower_ah = headers + voices[1]  # Second voice must be for left hand
+                lower_stream = abcFormat.translate.abcToStreamScore(lower_ah)
+                self.lower_d_part = DPart(music21_stream=lower_stream)
 
-    def get_voice_count(self):
-        if self.right_hand_d_part and self.left_hand_d_part:
+    def is_monophonic(self):
+        return self.combined_d_part.is_monophonic()
+
+    def get(self):
+        return self.combined_d_part
+
+    def get_upper(self):
+        return self.upper_d_part
+
+    def get_lower(self):
+        return self.upper_d_part
+
+    def get_stream(self):
+        return self.combined_d_part.get_stream()
+
+    def get_upper_stream(self):
+        if self.upper_d_part:
+            return self.upper_d_part.get_stream()
+        return None
+
+    def get_lower_stream(self):
+        if self.lower_d_part:
+            return self.lower_d_part.get_stream()
+        return None
+
+    def get_part_count(self):
+        if self.upper_d_part and self.lower_d_part:
             return 2
         return 1
 
@@ -79,7 +130,7 @@ class DCorpus:
     def __init__(self, corpus_path, corpus_type=Constant.CORPUS_ABC):
         self.d_scores = []
         if corpus_type == Constant.CORPUS_ABC:
-            abc_file = music21.abcFormat.ABCFile()
+            abc_file = abcFormat.ABCFile()
             abc_file.open(filename=corpus_path)
             abc_handle = abc_file.read()
             abc_file.close()
@@ -87,16 +138,15 @@ class DCorpus:
             for voice_id in ah_for_id:
                 d_score = DScore(abc_handle=ah_for_id[voice_id])
                 self.d_scores.append(d_score)
-
         else:
-            self.corpus = music21.converter.parse(corpus_path)
-            if isinstance(self.corpus, music21.stream.Opus):
+            self.corpus = converter.parse(corpus_path)
+            if isinstance(self.corpus, stream.Opus):
                 for score in self.corpus:
-                    d_score = DScore(music21_score=score)
+                    d_score = DScore(music21_stream=score)
                     self.d_scores.append(d_score)
             else:
                 score = self.corpus
-                d_score = DScore(music21_score=score)
+                d_score = DScore(music21_stream=score)
                 self.d_scores.append(d_score)
 
     def get_score_count(self):
--- test/test_d_corpus.py
+++ test/test_d_corpus.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
+from music21 import *
 import unittest
 import TestConstant
 from DCorpus.DCorpus import DPart, DScore, DCorpus
 
 BERINGER2_SCALE_CORPUS = TestConstant.BERINGER2_DIR + "/scales.abc"
 
+
 class DCorpusTest(unittest.TestCase):
     @staticmethod
-    def test_abc():
+    def test_abc_2part_mono():
         d_corpus = DCorpus(corpus_path=BERINGER2_SCALE_CORPUS)
         score_count = d_corpus.get_score_count()
         assert score_count == 38, "Bad score count"
         d_score = d_corpus.get_d_score_by_title("scales_g_minor_melodic")
         assert d_score is not None, "Bad score_by_title retrieval" 
-        voice_count = d_score.get_voice_count()
-        print("Voice count: {0}".format(voice_count))
-        assert voice_count == 2, "Bad voice count"
+        part_count = d_score.get_part_count()
+        assert part_count == 2, "Bad part count"
+        d_part = d_score.get()
+        assert d_part is not None, "Bad DPart retrieval"
+        assert d_part.is_monophonic() is False, "Polyphony not detected"
+        d_upper = d_score.get_upper()
+        assert d_upper, "Bad upper DPart"
+        assert d_upper.is_monophonic() is True, "Monophonic upper part not detected"
+        d_lower = d_score.get_lower()
+        assert d_lower, "Bad lower DPart"
+        assert d_lower.is_monophonic() is True, "Monophonic upper part not detected"
+
 
 
 if __name__ == "__main__":
