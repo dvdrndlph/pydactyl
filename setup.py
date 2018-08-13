482dd462ec22a79d30d5c8f23db8ec761c215754
David Randolph <dave@tuningbell.com>
2018-08-13 03:04:50 -0500

    Loaded to TestPyPI.

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='pydactyl',
-      version='0.1',
-      description='Toolkit for piano fingering. Part of Didactyl project at the University of Illinois at Chicago.',
-      url='http://github.com/dvdrndlph/pydactyl',
-      author='David A. Randolph',
-      author_email='drando2@uic.edu',
-      license='MIT',
-      packages=['pydactyl'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='pydactyl',
+    version='0.1',
+    description='Toolkit for piano fingering. Part of Didactyl project at the University of Illinois at Chicago.',
+    url='http://github.com/dvdrndlph/pydactyl',
+    author='David A. Randolph',
+    author_email='drando2@uic.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
