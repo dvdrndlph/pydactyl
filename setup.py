791c928edaae1f7c266aa02d5ce71eeebd484ba4
David Randolph <dave@tuningbell.com>
2018-08-13 02:25:52 -0500

    Package for PyPI.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pydactyl',
+      version='0.1',
+      description='Toolkit for piano fingering. Part of Didactyl project at the University of Illinois at Chicago.',
+      url='http://github.com/dvdrndlph/pydactyl',
+      author='David A. Randolph',
+      author_email='drando2@uic.edu',
+      license='MIT',
+      packages=['pydactyl'],
+      zip_safe=False)
